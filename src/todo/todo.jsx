9831a03d50b2d014326b911f9c9acc60c51843df
Adriano <adriano@Mac-mini-de-Adriano.local>
2018-10-31 12:15:49 -0300

    todo and about

---
+++ src/todo/todo.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Todo extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+    render() {
+        return (
+            <div>
+                <h1>Teste</h1>
+            </div>
+        )
+    }
+}
+
+export default Todo
