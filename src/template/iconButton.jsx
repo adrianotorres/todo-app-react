0b02f06ab243866ad8ab230956ce4ce1b84f8847
Adriano <adriano@Mac-mini-de-Adriano.local>
2018-11-01 11:22:52 -0300

    handleAddTodo

--- src/template/iconButton.jsx
+++ src/template/iconButton.jsx
@@ -4,7 +4,7 @@ import If from './if'
 
 export default props => (
     <If test={ !props.hide } >
-        <button className={ `btn btn-${ props.style }` } >
+        <button onClick={ props.onClick} className={ `btn btn-${ props.style }` } >
             <i className={ `fa fa-${ props.icon }` }></i>
         </button>
     </If>
--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -8,12 +8,16 @@ class Todo extends Component {
     constructor(props) {
         super(props)
         this.state = {}
+        this.handleAddTodo = this.handleAddTodo.bind(this)
+    }
+    handleAddTodo() {
+        console.log(this)
     }
     render() {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <TodoForm />
+                <TodoForm handleAdd={ this.handleAddTodo } />
                 <TodoList />
             </div>
         )
